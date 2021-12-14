d05b7d5c5e19d8eb3423728735d394ec35670748
Cjsah <2684117397@qq.com>
2021-12-14 22:51:18 +0800

    update part5

--- src/main/kotlin/net/cjsah/console/ConsoleCommand.kt
+++ src/main/kotlin/net/cjsah/console/ConsoleCommand.kt
@@ -17,8 +17,7 @@ object ConsoleCommand {
         // help
         register { dispatcher ->
             dispatcher.register(
-                literal("help").executes("打开帮助"
-                ) { context ->
+                literal("help").executes("打开帮助") { context ->
                     val helps = dispatcher.getHelp(context.getSource())
                     if (context.getSource() is ConsoleCommandSource) {
                         helps.entries.forEach { context.getSource().sendFeedBack("${it.key}\t${it.value}") }
@@ -48,8 +47,51 @@ object ConsoleCommand {
                 literal("permission").requires { source ->
                     (source.getSource() !is Contact || source.getSource() is User) &&
                             source.hasPermission(Permissions.PermissionType.ADMIN)
-                }.then(argument("plugin", PluginArgument.plugin())
-                    .then(literal("whitelist")
+                }.then(literal("reload")
+                    .executes("重载权限") { context ->
+                        permissions.reload()
+                        context.getSource().sendFeedBack("重载成功")
+                        Command.SUCCESSFUL
+                    }
+                ).then(argument("plugin", PluginArgument.plugin())
+                    .then(literal("set")
+                        .then(literal("whitelist")
+                            .executes("设置插件为白名单模式") { context ->
+                                val plugin = PluginArgument.getPlugin(context, "plugin")
+                                context.getSource().sendFeedBack(permissions.setListType(plugin, true))
+                                Command.SUCCESSFUL
+                            }
+                        ).then(literal("blacklist")
+                            .executes("设置插件为黑名单模式(默认)") { context ->
+                                val plugin = PluginArgument.getPlugin(context, "plugin")
+                                context.getSource().sendFeedBack(permissions.setListType(plugin, false))
+                                Command.SUCCESSFUL
+                            }
+                        )
+                    ).then(literal("status")
+                        .executes("权限状态") { context ->
+                            val plugin = PluginArgument.getPlugin(context, "plugin")
+                            var content = "插件 ${plugin.info.name} 的权限状态:\n"
+                            content += "模式: ${if (permissions.isWhite(plugin)) "白" else "黑"}名单\n"
+                            val wul = permissions.getWU(plugin).map { obj -> obj.toString() }
+                            val wgl = permissions.getWG(plugin).map { obj -> obj.toString() }
+                            val bul = permissions.getBU(plugin).map { obj -> obj.toString() }
+                            val bgl = permissions.getBG(plugin).map { obj -> obj.toString() }
+
+                            if (wul.isEmpty() && wgl.isEmpty()) content += "白名单: 空\n"
+                            else {
+                                if (wul.isNotEmpty()) content += "用户白名单: \n${wul.joinToString("\n")}\n"
+                                if (wgl.isNotEmpty()) content += "群白名单: \n${wgl.joinToString("\n")}\n"
+                            }
+                            if (bul.isEmpty() && bgl.isEmpty()) content += "黑名单: 空\n"
+                            else {
+                                if (bul.isNotEmpty()) content += "用户黑名单: \n${bul.joinToString("\n")}\n"
+                                if (bgl.isNotEmpty()) content += "群黑名单: \n${bgl.joinToString("\n")}\n"
+                            }
+                            context.getSource().sendFeedBack(content.substring(0, content.length-1))
+                            Command.SUCCESSFUL
+                        }
+                    ).then(literal("whitelist")
                         .then(literal("add")
                             .then(literal("user")
                                 .then(argument("id", LongArgument.longArg())
@@ -173,50 +215,7 @@ object ConsoleCommand {
                                 )
                             )
                         )
-                    ).then(literal("status")
-                        .executes("权限状态") { context ->
-                            val plugin = PluginArgument.getPlugin(context, "plugin")
-                            var content = "插件 ${plugin.info.name} 的权限状态:\n"
-                            content += "模式: ${if (permissions.isWhite(plugin)) "白" else "黑"}名单\n"
-                            val wul = permissions.getWU(plugin).map { obj -> obj.toString() }
-                            val wgl = permissions.getWG(plugin).map { obj -> obj.toString() }
-                            val bul = permissions.getBU(plugin).map { obj -> obj.toString() }
-                            val bgl = permissions.getBG(plugin).map { obj -> obj.toString() }
-
-                            if (wul.isEmpty() && wgl.isEmpty()) content += "白名单: 空\n"
-                            else {
-                                if (wul.isNotEmpty()) content += "用户白名单: \n${wul.joinToString("\n")}\n"
-                                if (wgl.isNotEmpty()) content += "群白名单: \n${wgl.joinToString("\n")}\n"
-                            }
-                            if (bul.isEmpty() && bgl.isEmpty()) content += "黑名单: 空\n"
-                            else {
-                                if (bul.isNotEmpty()) content += "用户黑名单: \n${bul.joinToString("\n")}\n"
-                                if (bgl.isNotEmpty()) content += "群黑名单: \n${bgl.joinToString("\n")}\n"
-                            }
-                            context.getSource().sendFeedBack(content.substring(0, content.length-1))
-                            Command.SUCCESSFUL
-                        }
-                    ).then(literal("set")
-                        .then(literal("whitelist")
-                            .executes("设置插件为白名单模式") { context ->
-                                val plugin = PluginArgument.getPlugin(context, "plugin")
-                                context.getSource().sendFeedBack(permissions.setListType(plugin, true))
-                                Command.SUCCESSFUL
-                            }
-                        ).then(literal("blacklist")
-                            .executes("设置插件为黑名单模式(默认)") { context ->
-                                val plugin = PluginArgument.getPlugin(context, "plugin")
-                                context.getSource().sendFeedBack(permissions.setListType(plugin, false))
-                                Command.SUCCESSFUL
-                            }
-                        )
                     )
-                ).then(literal("reload")
-                    .executes("重载权限") { context ->
-                        permissions.reload()
-                        context.getSource().sendFeedBack("重载成功")
-                        Command.SUCCESSFUL
-                    }
                 )
             )
         }
